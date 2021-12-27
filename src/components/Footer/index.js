b5fe312de2ca22316a4f866efcb7886bb0342953
Frankenstein <88062514+maxwillner@users.noreply.github.com>
2021-12-27 00:05:47 -0500

    added asset folder; added About component, Header component, Footer component

---
+++ src/components/Footer/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Footer() {
+
+    return (
+        <div>
+            <span>
+                <a href="https://github.com/maxwillner">GitHub</a>
+            </span>
+            <bk></bk>
+            <span>
+                <a href="https://www.linkedin.com/feed/">Linked</a> 
+            </span>
+        </div>
+    );
+}
+
+export default Footer;
---
+++ src/components/Header/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+function Header() {
+
+    return (
+        <header>
+            <h2>
+                <a href="/">Max Willner</a>
+            </h2>
+        </header>
+    )
+}
+
+export default Header; 
